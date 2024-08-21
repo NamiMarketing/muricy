4f5bf1786e0ffd052abc65c6f86e174eb6fcc90d
Marcelo Cho <marcelo.cho@gmail.com>
2024-08-21 09:22:26 -0300

    atualizacao site novo

--- .eleventy.js
+++ .eleventy.js
@@ -1,32 +1,14 @@
-const {
-  DateTime
-} = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css");
   eleventyConfig.addPassthroughCopy("./src/img");
-  eleventyConfig.addPassthroughCopy("./src/admin");
-
-  eleventyConfig.addFilter("postDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-  });
-
-  eleventyConfig.addNunjucksFilter("limit", (arr, limit) =>
-    arr?.slice(0, limit)
-  );
-
-  eleventyConfig.addCollection("semVideos", (collectionApi) => {
-    return collectionApi.getAll().filter((item) => {
-      if (item.data.layout == 'article.njk' && !item.data.tags?.some((el) => el === 'Vídeos')) {
-        return item.data;
-      }
-    });
-  });
 
   return {
     dir: {
       input: "src",
-      output: "public"
-    }
+      output: "public",
+    },
   };
 };
+
+
